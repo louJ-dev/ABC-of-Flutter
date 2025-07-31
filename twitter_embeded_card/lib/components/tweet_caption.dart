b750981229ed92d0e35520cbd0a412491128710b
louJ-dev <jericfactor@gmail.com>
2025-07-31 21:47:05 +0800

    add caption

---
+++ twitter_embeded_card/lib/components/tweet_caption.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TweetCaption extends StatelessWidget {
+  const TweetCaption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        style: TextStyle(
+          fontSize: 18,
+          color: Colors.black
+        ),
+        children: <TextSpan>[
+          TextSpan(text: "Did you know?\n"),
+          TextSpan(text: "\n"),
+          TextSpan(text:"When you call `MediaQuery.of(context)` inside a build method, the widget will rebuild when *any* of the MediaQuery properties change.\n"),
+          TextSpan(text: '\n'),
+          TextSpan(text: "But there's a better way that lets you depend only on the properties you care about (and minimize unnecessary rebuilds). 👇\n"),
+        ]
+      ),
+    );
+  }
+}
